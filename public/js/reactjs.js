b9ef15667f3986372f3563cc9f4780dbc1a2e419
da cuoi <buivanphuocforyou@gmail.com>
2017-04-14 08:17:01 +0700

    send mail

--- public/js/reactjs.js
+++ public/js/reactjs.js
@@ -26,7 +26,7 @@ var Tweet = React.createClass({
             <TwitterHeader />
             <TwitterPoster />
             <button onClick={() => this._getTweets()}>update</button>
-
+            <TwitterHeader />
             {handleTweets}
           </div>
       );
@@ -40,63 +40,63 @@ var TwitterHeader = React.createClass({
   }
 });
 
-    var TwitterPoster = React.createClass({
-      _handleClick: function() {
-          var tweetValue = this.refs.tweetvalue.value;
-          var token = $('meta[name="csrf-token"]').attr('content');
-          console.log(token);
-          console.log(tweetValue);
+var TwitterPoster = React.createClass({
+  _handleClick: function() {
+      var tweetValue = this.refs.tweetvalue.value;
+      var token = $('meta[name="csrf-token"]').attr('content');
+      console.log(token);
+      console.log(tweetValue);
 
-          $.post('/tweets', { tweet: tweetValue }, function(data) {
-              console.log(data);
-          });
-      },
-      render: function() {
-        return (
-          <div>
-              <input type="text" placeholder="your tweet ..." ref="tweetvalue" />
-              <input type="button" value="Tweet" onClick={this._handleClick} />
-          </div>
-        );
-      }
-    });
-
-    var TweetBanner = React.createClass({
-      getInitialState: function() {
-          return {
-              editinput: false
-          };
-      },
-      _removeItem: function() {
-          console.log(this.props.id);
-          $.post('/tweets/delete',{ id: this.props.id }, function(data) {
-              console.log(data);
-          });
-      },
-      _editItem: function() {
-          this.state.editinput ? this.setState({ editinput: false }) : this.setState({ editinput: true });
-      },
-      _handleSubmit: function() {
-          var editedValue = this.refs.editTweetValue.value;
-          $.patch('/tweets/' + this.props.id + '', { id: this.props.id, tweet: editedValue },function() {
-              this.setState({ editinput: false });
-          }.bind(this));
-      },
-      render: function() {
-          return(
-              <div>
-                  Tweet by Swagger :
-                  {this.props.tweet}
-                  {this.state.editinput ? <div> <input type="text"
-                      placeholder="edit ..." ref="editTweetValue" />
-                  <button onClick={this._handleSubmit} > Done </button> </div>: '' }
-                  <button onClick={this._editItem} > Edit </button>
-                  <button onClick={this._removeItem} > Remove </button>
-              </div>
-          );
-      }
-    });
-    ReactDOM.render(
-      <Tweet />,
-      document.getElementById('content')
+      $.post('/tweets', { tweet: tweetValue }, function(data) {
+          console.log(data);
+      });
+  },
+  render: function() {
+    return (
+      <div>
+          <input type="text" placeholder="your tweet ..." ref="tweetvalue" />
+          <input type="button" value="Tweet" onClick={this._handleClick} />
+      </div>
     );
+  }
+});
+
+var TweetBanner = React.createClass({
+    getInitialState: function() {
+        return {
+            editinput: false
+        };
+    },
+    _removeItem: function() {
+        console.log(this.props.id);
+        $.post('/tweets/delete',{ id: this.props.id }, function(data) {
+            console.log(data);
+        });
+    },
+    _editItem: function() {
+        this.state.editinput ? this.setState({ editinput: false }) : this.setState({ editinput: true });
+    },
+    _handleSubmit: function() {
+        var editedValue = this.refs.editTweetValue.value;
+        $.patch('/tweets/' + this.props.id + '', { id: this.props.id, tweet: editedValue },function() {
+            this.setState({ editinput: false });
+        }.bind(this));
+    },
+    render: function() {
+        return(
+            <div>
+                Tweet by Swagger :
+                {this.props.tweet}
+                {this.state.editinput ? <div> <input type="text"
+                    placeholder="edit ..." ref="editTweetValue" />
+                    <button onClick={this._handleSubmit} > Done </button> </div>: '' }
+                <button onClick={this._editItem} > Edit </button>
+                <button onClick={this._removeItem} > Remove </button>
+            </div>
+      );
+  }
+});
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('content')
+);
